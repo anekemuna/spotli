f107bb057923400c53608379234bf9fc93e11dda
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-11-17 15:42:42 -0700

    Add Empty HomePage and Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}/>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
     </Routes>
   );
 }
