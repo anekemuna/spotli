24ed6a4925137910c9699d8ba379d10599902b19
Munachimso Aneke <52995219+anekemuna@users.noreply.github.com>
2025-11-18 15:26:44 -0700

    Update SignUpPage

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const SignUpPage = () => {
+  const { signUp } = useAuth();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError ] = useState("");
 
-  const handleCreateAccount = () => {};
+  const navigate = useNavigate();
+
+  const handleCreateAccount = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await signUp(username, email, password);
+      navigate("/"); // redirect to home after signup
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="sign-up-page">
       <h1>Sign Up</h1>
-      <form>
-         <div className="username-container">
+      <form onSubmit={handleCreateAccount}>
+        <div className="username-container">
           <label htmlFor="username">Username:</label>
           <input
             type="username"
@@ -19,6 +39,7 @@ const SignUpPage = () => {
             placeholder="Enter username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            required
           />
         </div>
         <div className="email-container">
@@ -29,6 +50,7 @@ const SignUpPage = () => {
             placeholder="Enter email address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
         <div className="password-container">
@@ -39,12 +61,24 @@ const SignUpPage = () => {
             placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
+            minLength="6"
           />
         </div>
         <div>
-          <input type="submit" value="Create Account" onClick={handleCreateAccount} />
+          <input
+            type="submit"
+            value="Create Account"
+            disabled={loading}
+          />
         </div>
       </form>
+
+      <div className="error">{error && <p>{error}</p>}</div>
+
+      <div className="link-login">
+        Already have an account? <Link to="/login">Go to Login</Link>
+      </div>
     </div>
   );
 };
